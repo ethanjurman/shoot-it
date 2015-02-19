dc94970a586d0d267157d807aaaedc03a7c191b3
Wesley Wigham <wwigham@gmail.com>
2015-02-18 19:55:26 -0500

    draws path in space

--- host/game/generators/random.js
+++ host/game/generators/random.js
@@ -1,13 +1,12 @@
-var generators = require('./');
 var THREE = require('../libs/three');
 
-var VARIANCE = 50;
+var VARIANCE = 100;
 
 var Random = function(level, remaining) {
   var point = level.points[level.points.length-1].clone().add(new THREE.Vector3(Math.random(),Math.random(),Math.random()).multiplyScalar(VARIANCE));
   level.points.push(point);
-  if (remaining > 1) 
-    generators.Random(level, remaining - 1);
+  if (remaining > 0) 
+    Random(level, remaining - 1);
 };
 
 module.exports = Random;
