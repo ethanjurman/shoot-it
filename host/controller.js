4bb9131f116379e5398b294641b87a6b3f940f72
Tina Howard <chh2538@rit.edu>
2015-04-14 19:24:25 -0400

    added motion to the controller plane

--- host/controller.js
+++ host/controller.js
@@ -1,8 +1,7 @@
 
 var players = require('./game/player-store');
 var THREE = require('./game/libs/three');
-console.log("I am looking for controllers");
-var Controller = function() {
+var Controller = function(){
 
 }
 /*
@@ -12,7 +11,7 @@ go to game
 */
 
 //check for the pushed buttons
-function buttonPushed(controller){
+function buttonsPushed(controller){
 	var pushedButtons = [];
 	for(i in controller.buttons){
 		if(controller.buttons[i].pressed){
@@ -21,13 +20,20 @@ function buttonPushed(controller){
 	}
 	return pushedButtons;
 }
-setInterval(getGameInput, 500);
+
+function analogsInput(controller){
+	return controller.axes;
+// return array of analog input
+}
+
+setInterval(getGameInput, 16.66);
 var controllerPlayers = [false, false, false, false];
 
 function getGameInput(){
 	for(var i =0; i < controllerPlayers.length; i++){
+		var controller = navigator.getGamepads()[i];
 		//Adding a player to the controllerPlayers
-		if((!controllerPlayers[i]) && (navigator.getGamepads()[i] != undefined) && (buttonPushed(navigator.getGamepads()[i]).length > 0)){
+		if((!controllerPlayers[i]) && (controller != undefined) && (buttonsPushed(controller).length > 0)){
 			var player = {color: 0xff0000, playerId: -i};
 			var color = Number(player.color);
 			var ply = players.create(color);
@@ -36,6 +42,15 @@ function getGameInput(){
 			console.log("%c ADDED PLAYER "+ply.userId,"color: #"+color.toString(16));
 			ply.setPos(new THREE.Vector3(0,0,0));
 		}
+		//Providing the motion through the analog sticks
+		if(controllerPlayers[i]){
+			var motion = {y:analogsInput(controller)[0] * 14, z:analogsInput(controller)[1] * 14};
+			var id = -i;
+			var ply = players.find(id);
+  		if (!ply) throw new Error('Got update for nonexistant player id:'+ id +'');
+  		ply.applyMotion(motion);
+		}
+
 	}
 
 }
