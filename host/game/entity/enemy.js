6de4232f4e611330da332cd11a55d619c0a1e00d
Ethan H. Jurman <ehj2229@rit.edu>
2015-04-27 20:22:00 -0400

    better ships

--- host/game/entity/enemy.js
+++ host/game/entity/enemy.js
@@ -1,12 +1,15 @@
 var Damageable = require('./damageable');
 var THREE = require('../libs/three');
 var CANNON = require('../libs/cannon');
+var global = require('../global');
 var Bullet = require('./bullet');
 
-var Enemy = function(type) {
+var Enemy = function(type, path_point) {
+  // type = 1 - drone
   // type = 2 - firing ship
-  // type = 3 - drone
-  // type = 4 - boss?
+  // type = 3 - boss or mine?
+
+  // path point is an integer
   Damageable.call(this, 1);
   // base square ... not in use anymore?
   this.setGeometry(
@@ -20,11 +23,11 @@ var Enemy = function(type) {
   body.angularDamping = 0.5;
   this.setPhysicsBody(body);
   this.setGravity(0);
+  this.path_point = path_point;
   var self = this;
-  this.setModel('resources/ship'+type+'.obj','resources/ship'+type+'.mtl',function(){
+  this.setModel('resources/enemy_ship'+type+'.obj','resources/enemy_ship'+type+'.mtl',function(){
     self.mesh.scale.set(3,3,3);
   });
-  this.motion = null;
   this.firing = false; // when set to true, do fire action, then set to false
 };
 
@@ -32,9 +35,10 @@ Enemy.prototype = Object.create( Damageable.prototype );
 Enemy.prototype.userId = -1;
 
 Enemy.prototype.think = function() {
-  if (this.motion) {
-    // set position
-  }
+  // movement
+
+
+  // firing
   if (this.firing) {
     this.fireProjectile();
     this.firing = false;
