ae8014682c3dea0c042c524e7d7dfa59541d2ba4
Ethan H. Jurman <ehj2229@rit.edu>
2015-05-01 11:00:51 -0400

    players move slower and run-kinda if they haven't been added

--- host/socket.js
+++ host/socket.js
@@ -5,6 +5,7 @@ var socket = io();
 
 socket.on('add plane', function(player){
 	var color = Number(player.color);
+	localStorage[player.playerId] = player.color;
   var ply = players.create(color);
   ply.userId = player.playerId;
   console.log("%c ADDED PLAYER "+ply.userId,"color: #"+color.toString(16));
@@ -15,8 +16,16 @@ socket.on('update plane', function(data){
   var id = data[0],
       motion = data[1],
       ply = players.find(id);
-  if (!ply) throw new Error('Got update for nonexistant player id:'+ id +'');
-  ply.applyMotion(motion);
+  // if (!ply) throw new Error('Got update for nonexistant player id:'+ id +'');
+  if (!ply) {
+		// if the player doesn't exit, add them (and use the color we stored earlier)
+	  var color = localStorage[id] || 0xfffffff;
+		var ply = players.create(color);
+	  ply.userId = id;
+	  console.log("%c ADDED PLAYER "+ply.userId,"color: #"+color.toString(16));
+	  ply.setPos(new THREE.Vector3(0,0,0));
+	}
+	ply.applyMotion(motion);
 });
 
 socket.on('remove plane', function(id){
